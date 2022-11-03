6c38db07bb417b77deec25154025cbe925f6e83f
Joel Green <jg.althon@gmail.com>
2022-11-03 02:42:39 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './icons8-fiat-500.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
